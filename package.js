e9dc43f2893708b14c920530c8ec5a66232b5e3f
Nicolás López <nicolaslopezj@me.com>
2014-11-13 17:38:31 -0300

    Initial Commit

---
+++ package.js
@@ -0,0 +1,38 @@
+Package.describe({
+	name: 'orionjs:image-attribute',
+	summary: 'Image attribute form orionjs',
+	version: '0.0.1',
+	git: 'https://github.com/orionjs/image-attribute'
+});
+
+Package.onUse(function(api) {
+	api.versionsFrom('1.0');
+
+	api.use([
+		'orionjs:core',
+		'meteor-platform',
+		'less',
+		'aldeed:autoform@4.0.0-rc10', 
+		'lepozepo:s3@4.1.1', 
+		]);
+
+	api.imply([
+		'lepozepo:s3',
+		]);
+
+	api.addFiles([
+		'lib/attribute.js',
+		]);
+
+	api.addFiles([
+		'form/image.html',
+		'form/image.js',
+		'form/image.less',
+		'lib/autoform.js',
+		], 'client');
+});
+
+Package.onTest(function(api) {
+	api.use('tinytest');
+	api.use('orionjs:image-attribute');
+});

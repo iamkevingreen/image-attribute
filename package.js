62cb733eeb5b679fb426135f5c89d6a2dad2f9af
Nicolás López <nicolaslopezj@me.com>
2014-11-17 17:46:48 -0300

    Initial commit

--- package.js
+++ package.js
@@ -1,23 +1,27 @@
 Package.describe({
-	name: 'orionjs:image-attribute',
-	summary: 'Image attribute for orionjs',
+	name: 'orionjs:summernote',
+	summary: 'Summernote editor for orionjs',
 	version: '0.0.1',
-	git: 'https://github.com/orionjs/image-attribute'
+	git: 'https://github.com/orionjs/summernote'
 });
 
 Package.onUse(function(api) {
 	api.versionsFrom('1.0');
 
 	api.use([
-		'orionjs:core',
+		'orionjs:core@0.0.1',
 		'meteor-platform',
 		'less',
-		'aldeed:autoform@4.0.0-rc10', 
+		'aldeed:autoform@4.0.2', 
 		'lepozepo:s3@4.1.1', 
+		'nicolaslopezj:summernote@0.1.0',
+		'natestrauser:font-awesome@4.2.0',
+		'jquery@1.0.0'
 		]);
 
 	api.imply([
 		'lepozepo:s3',
+		'hckrs:summernote',
 		]);
 
 	api.addFiles([
@@ -25,9 +29,9 @@ Package.onUse(function(api) {
 		]);
 
 	api.addFiles([
-		'form/image.html',
-		'form/image.js',
-		'form/image.less',
+		'form/summernote.html',
+		'form/summernote.js',
+		'form/summernote.less',
 		'lib/autoform.js',
 		], 'client');
 });
